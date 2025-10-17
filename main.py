bc9272f5dab0175b8e940b3cae6e172b4a6deade
GouthamiNadella <gnadella@binghamton.edu>
2025-10-16 23:11:10 -0400

    Changing to new

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins
-    allow_credentials=False,  # Must be False when using allow_origins=["*"]
+    allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
 )
